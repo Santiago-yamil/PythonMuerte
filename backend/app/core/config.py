fd4459f537b8949fb5228f9131abfd2a0c8cfff7
Santiago Yamil <santiagoyamil2005@gmail.com>
2025-11-26 13:39:31 -0600

    Lectura de Imagenes lista

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,39 +1,33 @@
 #from pydantic import BaseSettings
-from pydantic_settings import BaseSettings
-from sqlalchemy import create_engine
-
-class Settings(BaseSettings):
-    APP_NAME: str = "Plataforma Integral"
-    OCR_PROVIDER: str = "tesseract"
-    AI_MODEL: str = "modelo_validacion_v1"
-    DATABASE_URL: str = "sqlite:///./database.db"
-
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
-
+#from pydantic_settings import BaseSettings
+#from sqlalchemy import create_engine
+#Remota============================================================================
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Motor y sesión de SQLAlchemy
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Cambia estos datos con los de tu servidor
+DB_USER = "hacka_ito"
+DB_PASSWORD = "hackaito2025"
+DB_HOST = "172.32.5.67"  
+DB_PORT = "5432"
+DB_NAME = "hackatec"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Crear motor de conexión
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# Crear sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para modelos
 Base = declarative_base()
 
-# Dependencia para endpoints
+# Función para obtener sesión (útil en Depends)
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-settings = Settings()
-from app.models.solicitud import Solicitud
-from app.models.apoyo import Apoyo
-# Importa aquí otros modelos si los hay
-
-Base.metadata.create_all(bind=engine)
--- backend/app/services/ocr_service.py
+++ backend/app/services/ocr_service.py
@@ -1,6 +1,11 @@
 import pytesseract
 from PIL import Image
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+pytesseract.pytesseract.TESSDATA_PREFIX = r"C:\Program Files\Tesseract-OCR\spa.traineddata"
+
+
 class OCRService:
     def extract_text(self, file_path: str) -> str:
         try:
